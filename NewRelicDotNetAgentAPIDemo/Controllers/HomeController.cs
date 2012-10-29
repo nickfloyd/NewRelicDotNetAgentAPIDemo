807d1777636bcb15908fb40d2bc3c25e923b4aba
Nick Floyd <nickfloyd@newrelic.com>
2012-10-28 22:55:10 -0700

    Adds RUM support for razor based views

--- NewRelicDotNetAgentAPIDemo/Controllers/HomeController.cs
+++ NewRelicDotNetAgentAPIDemo/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace NewRelicDotNetAgentAPIDemo.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Using the New Relic Agent API";
 
             return View();
         }
