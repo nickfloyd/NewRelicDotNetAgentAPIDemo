95eaf02d91e8e188935b4df37356a4dd97aa97b8
Nick Floyd <nickfloyd@newrelic.com>
2012-11-19 12:46:44 -0800

    removes dependency on sqllite and cleans up unused controllers

--- NewRelicDotNetAgentAPIDemo/Global.asax.cs
+++ NewRelicDotNetAgentAPIDemo/Global.asax.cs
@@ -22,7 +22,6 @@ namespace NewRelicDotNetAgentAPIDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
